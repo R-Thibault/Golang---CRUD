ddb1ba7d900c7242d99eefdd340718b007eb7f91
R-Thibault <thibault.rossa@gmail.com>
2024-09-17 09:18:51 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	initDB()
+	defer db.Close()
+
+	// route test
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Here is my API ! Welcome !")
+	})
+
+	http.HandleFunc("/books", getBooks) //GET all books
+	http.HandleFunc("/books/", getBookByID) //GET one book
+	http.HandleFunc("/books/create", createBook) //POST a new book
+	http.HandleFunc("/books/delete/", deleteBook) // DELETE a book
+	http.HandleFunc("/books/update/", updateBook) //UPDATE a book
+
+	// Start server
+	fmt.Println("Starting server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
